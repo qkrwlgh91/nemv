554de48ae387da2215d7c011031665aa1b937641
diamond <diamond@diamondui-MacBookPro.local>
2020-05-11 17:42:12 +0900

    project start

---
+++ be/routes/api/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var createError = require('http-errors');
+var router = express.Router();
+
+router.get('/hello', function(req, res, next) {
+  res.send({ msg: 'hello', b: 2 });
+});
+
+router.use('/test', require('./test'))
+
+router.all('*', function(req, res, next) {
+  next(createError(404, '존재하지 않는 api 입니다.'));
+});
+
+module.exports = router;
