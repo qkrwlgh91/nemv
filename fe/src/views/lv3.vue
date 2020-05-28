f700a16c13a6df625f19200088d7c631814c7e24
diamond <diamond@diamondui-MacBookPro.local>
2020-05-28 15:08:50 +0900

    momentjs add

--- fe/src/views/lv3.vue
+++ fe/src/views/lv3.vue
@@ -5,3 +5,23 @@
     </v-alert>
   </v-container>
 </template>
+<script>
+import moment from 'moment'
+
+export default {
+  mounted () {
+    // console.log(new Date()) 자바
+    console.log(moment().toDate()) // toDate()를 추가하면 new Date랑 같아지고 몽고디비에 저장시 이런형식으로 해야함
+    console.log(moment().format('YYYY/MM/DD'))
+    console.log(moment().minutes())
+    console.log(moment().add(3, 'hours').toString())
+    moment.locale('ko')
+    console.log(moment().fromNow())
+    console.log(moment().add(-5, 'hours').fromNow())
+    console.log(moment().subtract(5, 'hours').fromNow())
+    const bt = moment().add(-3, 'hours')
+    console.log(bt.diff(moment(), 'seconds'))
+    console.log(moment().diff(bt, 'seconds'))
+  }
+}
+</script>
